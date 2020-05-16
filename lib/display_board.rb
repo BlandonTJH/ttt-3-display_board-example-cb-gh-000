7b41362aa1c28313f4a279836261a5e33056466f
Blandon Tan <e170095@e.ntu.edu.sg>
2020-05-16 14:41:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
-puts "Welcome To Tic Tac Toe"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
-puts "   |   |   "# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+puts "   |   |   "
